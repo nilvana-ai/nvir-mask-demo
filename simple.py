b83b5210dabe7d4cbbd95f4b3b5aa01eb547e064
Taka Wang <jamie.w@inwinstack.com>
2022-07-30 22:15:47 +0800

    update docs and change response format

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'http://192.168.41.200:52010/v1/infer/111111111' # change this!
+url = 'http://127.0.0.1:52010/v1/infer/111111111' # change this!
 
 # inference
 files = {'image': open('assets/demo.jpg', 'rb')}
